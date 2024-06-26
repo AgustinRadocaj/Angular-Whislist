906c82c1f9fcf1f8327d44ff94d164b5a1df9445
Agustin Radocaj <jaguscaj@gmail.com>
2024-06-26 20:43:50 -0300

    Creacion y primera parte 54:59

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  wish: WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Build something awesome', true),
+    new WishItem('Go to Mars'),
+  ];
+
+  toogleWish(item: WishItem) {
+  item.isComplete = !item.isComplete;
+  console.log(item);
+  }
+
   title = 'my-angular-project';
 }
