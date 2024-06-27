bd99f13e4ff26049c89053139dd197228eeaebc1
Agustin Radocaj <jaguscaj@gmail.com>
2024-06-27 16:30:39 -0300

    ng model and getters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,6 +18,23 @@ export class AppComponent {
     new WishItem('Go to Mars'),
   ];
 
+  newWishText = '';
+  wishFilter: string = '0';
+  get items(): WishItem[] {
+    let value = this.wishFilter;
+    if(value === "0") {
+      return this.wish;
+    } else if (value === "1") {
+      return this.wish.filter(item => item.isComplete);
+    } else {
+      return this.wish.filter(item => !item.isComplete);
+    }
+  }
+
+  addWish() {
+    this.wish.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
   toogleWish(item: WishItem) {
   item.isComplete = !item.isComplete;
   console.log(item);
