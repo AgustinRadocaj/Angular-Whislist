2a99ec6b1b8e3a79433d43091f154bb35f0fc220
Agustin Radocaj <jaguscaj@gmail.com>
2024-07-03 17:36:10 -0300

    Reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { WishComponent } from './wish/wish.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
@@ -9,7 +10,7 @@ import { WishComponent } from './wish/wish.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, WishComponent],
+  imports: [RouterOutlet, FormsModule, WishComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
